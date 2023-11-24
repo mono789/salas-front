b78657ed7de28004ed9b0f8158db82cfe75425bf
Sneyder Yepes <apoyoit.ingenia@udea.edu.co>
2023-11-24 03:14:22 +0000

    Configuración despliegue ambiente develop

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  basePath: '/salasinfo',
+  output: 'export',
+  distDir: 'dist'
+}
 
 module.exports = nextConfig
