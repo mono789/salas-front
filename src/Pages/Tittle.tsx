4a0d978439d801ff508e946b0c6390ccf4ed0d5a
NATALIA HOYOS MARIN <natalia.hoyosm@udea.edu.co>
2023-12-11 12:41:14 -0500

    Feat: Diseño semanal y Salas Disponibles

---
+++ src/Pages/Tittle.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const Tittle = () => {
+   return (
+    <h1 className="bg-success nav-link text-white fs-5" aria-current="page">
+        Salas Disponibles
+    </h1>
+   )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,17 +2,19 @@ import SidebarMenu from "../Layout/SidebarMenu";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 import { Index } from "@/Pages/Index";
+import { Tittle } from "@/Pages/Tittle";
 
 export default function Home() {
   return (
-    <div className="container-left">
+    <div className="container-center">
       <div className="row">
-        <div className="col-8">
+        <div className="col-6">
           <SidebarMenu />
         </div>
-        <div className="col-4">
+        <div className="col-6">
+          <Tittle />
           <Index />
-        </div>
+          </div>
       </div>
     </div>
   );
