59bc3463ca42f6b19fc5969f136cb2b5add2572b
NATALIA HOYOS MARIN <natalia.hoyosm@udea.edu.co>
2023-12-11 12:42:34 -0500

    Feat: Especificación semanal

--- src/Pages/Tittle.tsx
+++ src/Pages/Tittle.tsx
@@ -3,7 +3,7 @@ import React from "react";
 export const Tittle = () => {
    return (
     <h1 className="bg-success nav-link text-white fs-5" aria-current="page">
-        Salas Disponibles
+        Salas Disponibles (Febrero 12-17)
     </h1>
    )
 }
