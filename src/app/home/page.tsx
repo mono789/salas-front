0ee42069a557ee31efcc63c87be03c3539718316
Apoyo desarrollo Ingenieria 1 <apoyodesarrolloingenieria1@udea.edu.co>
2024-09-10 00:29:53 -0500

    add structure to /home route

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,38 +1,115 @@
-import RoomCard from '@/components/RoomCard'
-import React from 'react'
+"use client";
 
-const roomsData = [{ blockNumber: "Bloque 20", roomNumber: "20-331" },
-  { blockNumber: "Bloque 20", roomNumber: "20-335" },];
+import RoomCard from "@/components/RoomCard";
+import React, { useState } from "react";
 
+const roomsData = [
+  { blockNumber: "Bloque 20", roomNumber: "20-331" },
+  { blockNumber: "Bloque 20", roomNumber: "20-335" },
+  { blockNumber: "Bloque 20", roomNumber: "20-335" },
+  { blockNumber: "Bloque 20", roomNumber: "20-335" },
+  { blockNumber: "Bloque 20", roomNumber: "20-335" },
+  { blockNumber: "Bloque 20", roomNumber: "20-335" },
+];
 
 const Page = () => {
+  const [menuAvanzado, useMenuAvanzado] = useState(false);
+  const [selectedOptions, useSelectedOptions] = useState(Array<string>);
+
+  const useToggleMenuAvanzado = () => {
+    useMenuAvanzado(!menuAvanzado);
+  };
+
+  const useAddSelectedOptions = (htmlElement) => {
+    const newList = selectedOptions.slice();
+
+    if (newList.includes(htmlElement.target.id)) {
+      const index = newList.indexOf(htmlElement.target.id);
+      newList.splice(index, 1);
+    } else {
+      newList.push(htmlElement.target.id);
+    }
+    useSelectedOptions(newList);
+    console.log(newList);
+  };
+
   return (
     <div>
-      <div style={{ display: 'flex' }}>
-        <p>Esta página contiene funciones e información de interés general<br></br>
-        De manera que lo que está aquí no hay que reservarlo a ningún usuario en particular.<br></br>
-        Algunas de estas funciones son: Ver y filtrar salones por criterios generales,<br></br>
-        poder visualizar al interior de estos salones la información general de estos.
-        Esto incluye: software, horarios??, etc. 
-        </p>
+      <div className="flex flex-col justify-start p-3">
+        <div className="w-full mt-4">
+          <input
+            className="block w-full px-4 py-2 mt-2 placeholder-gray-400  backdrop-blur-lg border border-gray-200 rounded-lg dark:bg-gray-800 dark:border-gray-600 dark:placeholder-gray-400 focus:border-blue-400 dark:focus:border-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring focus:ring-blue-300"
+            type="text"
+            placeholder="Filtro más importante"
+          />
 
-        {/*
-        Legacy code a optimizar:
-
-        <Sidebar />   Contiene acciones generales de la pantalla, debería poderse customizar dependiendo del usuario
-        pero voy a intentar de momento poner lo esencial aquí. Si algo se define una por usuario xd
-        
-        
-        
-        
-        */}
-
+          <div className="flex justify-between my-1.5">
+            {
+              // Botón para desplegar el menú avanzado del filtro
+            }
+            <button
+              x-cloak="true"
+              className="m-2 text-gray-500 dark:text-gray-200 hover:text-gray-600 dark:hover:text-gray-400 focus:outline-none focus:text-gray-600 dark:focus:text-gray-400"
+              aria-label="toggle menu"
+              onClick={useToggleMenuAvanzado}
+            >
+              <svg
+                x-show="!isOpen"
+                xmlns="http://www.w3.org/2000/svg"
+                className="w-6 h-6"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                strokeWidth="2"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  d="M4 8h16M4 16h16"
+                />
+              </svg>
+            </button>
+            {
+              // TODO: Los botones de filtros son candidatos para volverlos componente
+            }
+            <button
+              id="salones-libres"
+              className={`p-2 bg-lime-500 dark:bg-teal-950 ${
+                selectedOptions.includes("salones-libres")
+                  ? "opacity-100"
+                  : "opacity-70"
+              } border-2 rounded-md`}
+              onClick={useAddSelectedOptions}
+            >
+              Salones libres
+            </button>
+            <button
+              id="salones-ocupados"
+              className={`p-2 bg-lime-500 dark:bg-teal-950 ${
+                selectedOptions.includes("salones-ocupados")
+                  ? "opacity-100"
+                  : "opacity-70"
+              } duration-100 dark:bg-teal-950 border-2 rounded-md`}
+              onClick={useAddSelectedOptions}
+            >
+              Salones ocupados
+            </button>
+          </div>
+          {menuAvanzado ? ( // También rediseñar esto
+            <div>menú avanzado</div>
+          ) : (
+            <></>
+          )}
+        </div>
         <div>
           <section className="text-gray-600 body-font">
-            <div className="container px-5 py-24 mx-auto">
+            <div className="container px-5 py-10 lg:py-24 mx-auto">
               <div className="flex flex-wrap -m-4">
                 {roomsData.map((room, index) => (
-                  <div key={index} className="lg:w-1/4 md:w-1/2 p-4 w-full">
+                  <div
+                    key={index}
+                    className="flex justify-center lg:w-1/4 md:w-1/2 p-4 w-full"
+                  >
                     <RoomCard
                       blockNumber={room.blockNumber}
                       roomNumber={room.roomNumber}
@@ -45,7 +122,7 @@ const Page = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Page
+export default Page;
