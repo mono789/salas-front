3d68747a98f0f93ee12d7be5577a982bbcfbd58a
Apoyo desarrollo Ingenieria 1 <apoyodesarrolloingenieria1@udea.edu.co>
2024-10-06 17:51:45 -0500

    Flat emptySchedule route

--- src/app/emptySchedule/page.tsx
+++ src/app/emptySchedule/page.tsx
@@ -1,7 +1,9 @@
 import React from "react";
-import Table from "./table";
+import Table from "../../components/table";
 
 const Page = () => {
+  const user = { rol: "Estudiante" };
+
   return (
     <div className="flex flex-col min-h-screen">
       <div
@@ -11,8 +13,8 @@ const Page = () => {
         }}
       >
         <h2 className="text-center mb-4 mt-4 text-lg font-semibold">
-          ¡Hola docente! Haz clic en el horario que se acomode a sus necesidades
-          para ver a detalle la sala que esté libre.
+          ¡Hola {user.rol}! Haz clic en el horario que se acomode a tus
+          necesidades para ver a detalle la sala que esté libre.
         </h2>
         <div className="w-11/12 sm:w-10/12 md:w-9/12 lg:w-8/12 xl:w-7/12 px-4">
           <Table />
