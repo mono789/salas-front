1dc712f48c03ab9030b9487aa0cf27f4799ebb66
GerarC <gcastillo.m14@gmail.com>
2024-10-21 14:50:53 -0500

    fix: RoomDescription errors

--- src/app/admin/reservation/page.tsx
+++ src/app/admin/reservation/page.tsx
@@ -1,9 +1,24 @@
+"use client";
 import React from "react";
 import Form from "./form";
 import RoomDescription from "@/components/description";
+import { SpecificRoomResponse } from "@/models/room";
 
 /* TODO: Esta página puede que tenga más uso en el área de usuario. En las rutas regulares de salasinfo.
 /  Podría tener uso en las rutas de las salas, para generar la reserva de las mismas.*/
+
+const room: SpecificRoomResponse = {
+  id: 1,
+  computerAmount: 1,
+  building: "20",
+  roomNum: "20",
+  roomName: "Sala de recreo",
+  software: [{ id: 1, name: "Krita", version: "2.0.0" }],
+  restrictions: [{ id: 1, description: "No se puede caminar en las mesas" }],
+  implements: [
+    { id: 1, name: "Tablero", state: "Más dañado que baño público" },
+  ],
+};
 
 const Page = () => {
   return (
@@ -16,12 +31,8 @@ const Page = () => {
       >
         <Form />
         <RoomDescription
-          nombre="dummy"
-          restricciones="ninguna"
-          nEquipos="20"
-          implementos="dummy"
-          os="Windows"
-          software="Word"
+          room={room}
+          onReservationButtonClick={() => console.log("ola")}
         />
       </div>
     </div>
--- src/utils/constants/api.constants.ts
+++ src/utils/constants/api.constants.ts
@@ -1,20 +1,21 @@
 import { Methods } from "@/services/api/service.types";
 
-export const TOKEN_TYPE = 'Bearer'
+export const TOKEN_TYPE = "Bearer";
 
-export const DEFAULT_API_URL = 'https://arquimedes.udea.edu.co:8091/salasinfo/v1'
+export const DEFAULT_API_URL =
+    "https://arquimedes.udea.edu.co:8091/salasinfo/v1";
 
-export const AUTHENTICATION_ENDPOINT = '/auth'
-export const USERS_ENDPOINT = '/users'
-export const ROOMS_ENDPOINT = '/rooms'
-export const RESERVATIONS_ENDPOINT = '/reservations'
+export const AUTHENTICATION_ENDPOINT = "/auth";
+export const USERS_ENDPOINT = "/users";
+export const ROOMS_ENDPOINT = "/rooms";
+export const RESERVATIONS_ENDPOINT = "/reservations";
 
 export const METHOD: Methods = {
     get: "GET",
-    post: "POST",
-    delete: "DELETE",
     put: "PUT",
+    post: "POST",
     patch: "PATCH",
+    delete: "DELETE",
 };
 
 export const BASIC_HEADER = {
