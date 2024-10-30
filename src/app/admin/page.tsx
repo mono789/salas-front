9f5c4c0023f23ceb83e50d869da87c261f9b216c
mono789 <juan.rivera7@udea.edu.co>
2024-10-30 17:56:00 -0500

    Creación de reservas y clases funcionales

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,15 @@
+"use client";
+
+import React from 'react';
+import Page from '../home/page';
+
+const AdminPage = () => {
+  return (
+    <div>
+      <h1>Panel de Administración</h1>
+      <Page/>
+    </div>
+  );
+};
+
+export default AdminPage;
--- src/app/home/room/page.tsx
+++ src/app/home/room/page.tsx
@@ -1,10 +1,15 @@
 "use client";
 import RoomDescription from "@/components/description";
 import ReservationModal from "@/components/modals/ReservationModal";
+import RegisterClassModal from "@/components/modals/RegisterClassModal";
+
 import { RoomScheduleResponse, SpecificRoomResponse } from "@/models/room";
 import RoomService from "@/services/api/room.service";
 import { useSearchParams } from "next/navigation";
 import { useEffect, useState } from "react";
+import { LocalStorageService } from "@/services/localstorage/local-storage.service";
+import UserService from "@/services/api/user.service";
+import { UserResponse } from "@/models/user";
 
 const ROOM_ID_PARAM = "id";
 
@@ -14,6 +19,35 @@ const Page = () => {
   const [schedule, setSchedule] = useState<RoomScheduleResponse>();
   const [openReservationModal, setOpenReservationModal] =
     useState<boolean>(false);
+  const [openRegisterClassModal, setOpenRegisterClassModal] =
+  useState<boolean>(false);
+  const [userData, setUserData] = useState<UserResponse>();
+
+  useEffect(() => {
+    const fetchUser = async () => {
+      try {
+        const storedUser = LocalStorageService.getItem("user") as UserResponse;
+        console.log("OLD USER DATA: ", userData);
+  
+        if (storedUser?.id) {
+          // Si hay un usuario almacenado con ID, obtenemos la información del usuario
+          const userResponse = await UserService.getOne(storedUser.id).then((response) => response.json());
+          const role = userResponse.role.roleName;
+          setUserData(userResponse);
+          console.log("NEW DATA:", userData);
+          console.log("USER RESPONSE: ", userResponse);
+        } else {
+          setUserData(storedUser);
+        }
+      } catch (error) {
+        console.error("Error al obtener el usuario:", error);
+      }
+    };
+  
+    fetchUser();
+    // La lista de dependencias está vacía para que el efecto solo se ejecute una vez al montar
+  }, []);
+  
 
   useEffect(() => {
     const id = params.get(ROOM_ID_PARAM);
@@ -44,13 +78,24 @@ const Page = () => {
         onReservationButtonClick={() =>
           setOpenReservationModal(!openReservationModal)
         }
+        userData={userData}
+        onRegisterClassButtonClick={() =>
+          setOpenRegisterClassModal(!openRegisterClassModal)}
       />
       <ReservationModal
         opened={openReservationModal}
         setOpened={setOpenReservationModal}
         saveReservation={(res) => {}}
         room={room}
+        userData={userData}
       ></ReservationModal>
+      <RegisterClassModal
+        opened={openRegisterClassModal}
+        setOpened={setOpenRegisterClassModal}
+        saveReservation={(res) => {}}
+        room={room}
+        userData={userData}
+      ></RegisterClassModal>
       <div className="flex flex-col place-items-center bg-white dark:bg-gray-800 rounded-lg shadow-lg mt-5 md:ml-5 p-2">
         {schedule ? (
           <>
