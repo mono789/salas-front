52def9c91b93ce585c28027726fafb86de041ff0
Kev <kevin.estrada1@udea.edu.co>
2024-10-31 14:03:44 -0500

    Vista de Home Cambiada

--- src/app/admin/reservation/page.tsx
+++ src/app/admin/reservation/page.tsx
@@ -3,12 +3,24 @@ import React from "react";
 import Form from "./form";
 import RoomDescription from "@/components/description";
 import { SpecificRoomResponse } from "@/models/room";
-import Page from '../../home/room/[sala]/page';
 
 /* TODO: Esta página puede que tenga más uso en el área de usuario. En las rutas regulares de salasinfo.
 /  Podría tener uso en las rutas de las salas, para generar la reserva de las mismas.*/
 
-const RoomReservation = () => {
+const room: SpecificRoomResponse = {
+  id: 1,
+  computerAmount: 1,
+  building: "20",
+  roomNum: "20",
+  roomName: "Sala de recreo",
+  software: [{ id: 1, name: "Krita", version: "2.0.0" }],
+  restrictions: [{ id: 1, description: "No se puede caminar en las mesas" }],
+  implements: [
+    { id: 1, name: "Tablero", state: "Más dañado que baño público" },
+  ],
+};
+
+const Page = () => {
   return (
     <div>
       <div
@@ -17,10 +29,14 @@ const RoomReservation = () => {
           backgroundImage: "url('/assets/bottom.png')",
         }}
       >
-        <Page />
+        <Form />
+        <RoomDescription
+          room={room}
+          onReservationButtonClick={() => console.log("ola")}
+        />
       </div>
     </div>
   );
 };
 
-export default RoomReservation;
+export default Page;
